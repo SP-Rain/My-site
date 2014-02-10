68393e18f893d160b2b04ad8fabb3e5de033e524
SP-Rain <SP-Rain@ya.ru>
2014-02-10 14:16:12 +0400

    little add for menu

--- functions.php
+++ functions.php
@@ -1,3 +1,11 @@
 <?php
 	add_theme_support ('menus');
 ?>
+<?php 
+	function my_wp_nav_menu_args ($args=''){
+	$args ['container']='';
+	$args ['depth']='0';
+	return $args;
+}
+?> <!-- this code automatically sets the value of
+specified elements of $args for all the menus on the site -->
